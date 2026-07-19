d5f76454f95547e2da7b84f38b4e0b45a2bc8fbb
naveenbm11-glitch <mysorenaveen@gmail.com>
2026-07-19 22:19:32 +0530

    Add files via upload

---
+++ myAssignments/Assignment1.js
@@ -0,0 +1,19 @@
+var firstName="Naveen";
+var companyName = "Cognizant"; 
+var mobileNumber = 1234567890;
+var isAutomation = false;
+var hasPlaywright; 
+console.log(typeof firstName+ " "+firstName);
+console.log(typeof companyName+ " "+companyName);
+console.log(typeof mobileNumber+ " "+mobileNumber);
+console.log(typeof isAutomation+ " "+isAutomation);
+console.log(typeof hasPlaywright+ " "+hasPlaywright);
+
+
+var firstName, companyName, mobileNumber, isAutomation, hasPlaywright;
+firstName = "Naveen";
+companyName = "Cognizant";
+mobileNumber = 1234567890;
+isAutomation = false;
+hasPlaywright = undefined;
+console.log(typeof firstName, typeof companyName, typeof mobileNumber, typeof isAutomation, typeof   hasPlaywright);
---
+++ myAssignments/Assignment2.js
@@ -0,0 +1,11 @@
+
+for (let j = 1; j <= 20; j ++) {
+if (j%2!=0){
+  console.log(j); 
+}
+}
+
+let a='7';
+let b= 7;
+console.log(a==b);
+console.log(a===b);
